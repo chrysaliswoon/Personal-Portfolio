13e9f5c3f8984aa36b291b275b63eea932e64f71
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2023-06-01 12:07:14 +0800

    Add assets

--- personal-portfolio/src/components/Skills.js
+++ personal-portfolio/src/components/Skills.js
@@ -1,11 +1,9 @@
-// import meter1 from "../assets/img/meter1.svg";
-// import meter2 from "../assets/img/meter2.svg";
-// import meter3 from "../assets/img/meter3.svg";
+import webdevelopment from "../assets/icons/web.png";
+import mern from "../assets/icons/mern.png";
+import mean from "../assets/icons/mean.png";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-// import arrow1 from "../assets/img/arrow1.svg";
-// import arrow2 from "../assets/img/arrow2.svg";
-// import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -38,20 +36,16 @@ export const Skills = () => {
                         <p>Here are some of the skills I've picked up over the years:</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                {/* <img src={meter1} alt="Image" /> */}
+                                <img src={webdevelopment} alt="front-end stack" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                {/* <img src={meter2} alt="Image" /> */}
-                                <h5>Brand Identity</h5>
+                                <img src={mern} alt="Mern Stack" />
+                                <h5>MERN Stack</h5>
                             </div>
                             <div className="item">
-                                {/* <img src={meter3} alt="Image" /> */}
-                                <h5>Logo Design</h5>
-                            </div>
-                            <div className="item">
-                                {/* <img src={meter1} alt="Image" /> */}
-                                <h5>Web Development</h5>
+                            <img src={mean} alt="Mean Stack" />
+                                <h5>MEAN Stack</h5>
                             </div>
                         </Carousel>
                     </div>
