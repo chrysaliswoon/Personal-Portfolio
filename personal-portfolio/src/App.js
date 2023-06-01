166262047a02c96eb7c66407b7754ed802abb67b
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2023-06-01 10:47:46 +0800

    Added components

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-
+      <Banner></Banner>
+      <Skills></Skills>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
